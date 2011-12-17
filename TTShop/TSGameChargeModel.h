69585da0513f6e4a19e49ae3bda50a7753890040
StevenLi <steven.php@gmail.com>
2011-12-18 01:32:55 +0800

    提交

--- TTShop/TSGameChargeModel.h
+++ TTShop/TSGameChargeModel.h
@@ -8,6 +8,25 @@
 
 #import <Foundation/Foundation.h>
 
+
 @interface TSGameChargeModel : NSObject
+{
+    BOOL					is_loading;
+    NSArray                 *buffer_games;
+    NSArray                 *buffer_values;
+    
+    id                      delegate;
+}
+
+@property (nonatomic, readonly) BOOL is_loading;
+@property (nonatomic, readonly) NSArray *buffer_games;
+@property (nonatomic, readonly) NSArray *buffer_values;
+@property (nonatomic, assign) id delegate;
+
+- (TSGameChargeModel *) initWithDelegate:(id)aDelegate;
+
+- (void) loadData;
+
+- (void) onLoadData:(NSNotification *)notify;
 
 @end
--- TTShop/TSGameChargeModel.m
+++ TTShop/TSGameChargeModel.m
@@ -7,7 +7,104 @@
 //
 
 #import "TSGameChargeModel.h"
+#import "TSConfigs.h"
+#import "STURLLoader.h"
 
 @implementation TSGameChargeModel
+
+@synthesize is_loading;
+@synthesize buffer_games;
+@synthesize buffer_values;
+@synthesize delegate;
+
+- (TSGameChargeModel *) initWithDelegate:(id)aDelegate
+{
+    self = [super init];
+    
+    if (self)
+    {
+        is_loading = NO;
+        delegate = aDelegate;
+    }
+    
+    return self;
+}
+
+- (void) dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+	[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    
+    [STURLLoader releaseBindedLoaderForDelegate:self];
+    
+    [buffer_games release];
+    [buffer_values release];
+    
+    [super dealloc];
+}
+
+- (void) loadData
+{
+    if (is_loading)
+	{
+		return;
+	}
+    
+    is_loading = YES;
+	[UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    
+    NSString *full_url = [NSString stringWithFormat:@"%@%@", BASE_URL, nil];
+    
+    STURLRequest *req = [[STURLRequest alloc] initWithURLString:full_url];
+    STURLLoader *loader = [[STURLLoader alloc] initWithURLRequest:req];
+    [loader addEventListener:STLOADER_COMPLETE target:self action:@selector(show:)];
+    
+    [req release];
+    [STURLLoader bindLoader:loader withDelegate:self];
+    
+    [loader load];
+}
+
+- (void) onLoadData:(NSNotification *)notify
+{
+    is_loading = NO;
+	[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    
+    STURLLoader *loader = (STURLLoader *)notify.object;
+    NSDictionary *json = [loader getJSONData];
+    
+    [loader removeEventListener:STLOADER_FAIL target:self];
+	[loader removeEventListener:STLOADER_COMPLETE target:self];
+	[loader release];
+    
+    [STURLLoader releaseBindedLoaderForDelegate:self];
+    
+    if (buffer_games)
+    {
+        [buffer_games release];
+        buffer_games = nil;
+    }
+    
+    buffer_games = [NSArray arrayWithObjects:@"联众游戏", @"QQ游戏", @"GT赛车", nil];
+    
+    if (buffer_values)
+    {
+        [buffer_values release];
+        buffer_values = nil;
+    }
+    
+    buffer_values = [NSArray arrayWithObjects:
+                     [NSArray arrayWithObjects:@"10元", @"100元", nil],
+                     [NSArray arrayWithObjects:@"5点", @"99点", @"123点", nil],
+                     [NSArray arrayWithObjects:@"100Kala", @"200kala", @"300kala", nil],
+                     nil];
+    
+    
+        //call delegate method
+    if (delegate && [delegate respondsToSelector:@selector(onModelLoaded:)])
+    {
+        [delegate performSelector:@selector(onModelLoaded:) withObject:self];
+    }
+}
 
 @end
